43e34bfb56f2ffec3a742917243ef2bedc296b18
Juan Oliveira <ruantos@proton.me>
2025-07-16 00:55:47 -0300

    created new appjs

---
+++ logica-js-projeto_inicial/app_a.js
@@ -0,0 +1,23 @@
+// Pop up na tela  
+alert('Boas vindas ao nosso site!');
+
+// Atribuição de var
+let nome = 'Lua';
+let idade = 25;
+let numero_vendas = 50;
+let saldo_disponivel = 1000;
+
+let mensagem_erro = 'Erro! Preencha todos os campos';
+alert(mensagem_erro);
+
+// Input de dados
+nome = prompt('Qual o seu nome mesmo? ');
+idade = prompt('Esqueci sua idade. Digite-a novamente: ');
+
+// Condicionais
+if (idade >= 18){
+    console.log('Já pode tirar habilitação! Mas não conta para a polícia rs');
+}
+else {
+    console.log('Vá dormir, criança');
+}
